e75ded2f06c247114f9d040a88cdbc318d02ec28
Nikolai <kalistratov@live.com>
2017-11-10 21:17:54 +0100

    Created GET  / POST posts request Handlers

--- PostController.php
+++ PostController.php
@@ -35,5 +35,57 @@ RequestService::TokenCheck();
 // Get token value from REQUEST
 $token = RequestService::GetToken();
 
+// CREATE PostsRepository
+$postRepository = new PostsRepository();
+
+// HANDLE REQUEST
+switch ($method){
+
+    //--------------------------------------------------------------------------
+    // GET POSTS
+    //--------------------------------------------------------------------------
+    case 'GET':
+
+        $postsAmount = isset($_GET['amount']) && !empty($_GET['amount']);
+        $postsByUser = isset($_GET['username']) && !empty($_GET['username']);
+
+        // RETURN POST BY SPECIFIC USER
+        if($postsByUser){
+
+            // get posts by user
+            $posts = $postRepository->getPostsByUser($token, $_GET['username']);
+
+            echo json_encode( $posts, JSON_UNESCAPED_UNICODE );
+            die;
+        }
+        
+        // RETURN ALL POSTS
+        if($postsAmount){
+
+            $posts = $postRepository->getPosts($token, $_GET['amount']);
+
+            echo json_encode( $posts, JSON_UNESCAPED_UNICODE );
+            die;
+        }
+            
+    break;
+
+    //--------------------------------------------------------------------------
+    // CREATE POST
+    //--------------------------------------------------------------------------
+    case 'POST':
+
+        $sRequestBody = file_get_contents('php://input');
+        $jRequestBody = RequestService::ParseRequestBody($sRequestBody);
+
+        if($jRequestBody === 'corrupted'){
+            ResponseService::ResponseBadRequest();
+        }
+        else{
+            ResponseService::ResponseOk("Ok Request");
+        }
+
+    break;
+}
 
 ?>
