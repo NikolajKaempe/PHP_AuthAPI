63ee7797184f12ca3bb8f6fcd05d7ecd2abd00f9
Nikolai <kalistratov@live.com>
2017-11-10 21:16:06 +0100

    Post Controller Refactoring

--- PostController.php
+++ PostController.php
@@ -1,22 +1,36 @@
 <?php
 
-include_once('Entities/AuthToken.php');
-include_once('Logic/Validation.php');
 include_once('./Services/RequestService.php');
+include_once('./Services/ResponseService.php');
+include_once('./Repositories/PostsRepository.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
 $input = file_get_contents('php://input');
 
+
+//------------------------------------------------------------------------------
 /*
-    Checks if REQUEST contains:
+
+    RequestService::TokenCheck() Checks if REQUEST contains:
         Authorization Header
         Valid Token
 
-        if on of the check failed -> 
-        function response with NotAuthorized 401
-        AND the below functions will not be called
+            if TokenCheck() failed -> 
+            Service response to client with Not Authorized 401
+            AND the below functions will not be executed
 */
+//------------------------------------------------------------------------------
+
 RequestService::TokenCheck();
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+
+/*
+ *
+    The Below Code will be Executed ONLY IF REQUEST CONTAINS A VALID TOKEN
+*/
 
 // Get token value from REQUEST
 $token = RequestService::GetToken();
