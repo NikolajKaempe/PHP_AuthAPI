2db616b3dd541c4ab2485f940e7a0afe2f5a452a
Nikolai <kalistratov@live.com>
2017-11-10 18:23:52 +0100

    Added Token Check to PostController

--- PostController.php
+++ PostController.php
@@ -2,8 +2,24 @@
 
 include_once('Entities/AuthToken.php');
 include_once('Logic/Validation.php');
+include_once('./Services/RequestService.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
 $input = file_get_contents('php://input');
+
+/*
+    Checks if REQUEST contains:
+        Authorization Header
+        Valid Token
+
+        if on of the check failed -> 
+        function response with NotAuthorized 401
+        AND the below functions will not be called
+*/
+RequestService::TokenCheck();
+
+// Get token value from REQUEST
+$token = RequestService::GetToken();
+
 
 ?>
