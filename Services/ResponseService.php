eabf9e561f2f90081641021e049807c2a26f0d3a
Nikolai <kalistratov@live.com>
2017-11-10 21:09:35 +0100

    Made response message optional

--- Services/ResponseService.php
+++ Services/ResponseService.php
@@ -3,22 +3,42 @@ class ResponseService
 {
     
     // Responds with 200 status and message
-    public static function ResponseOk($msg)
+    public static function ResponseOk($msg = "undefined")
     {
         header("HTTP/1.1 200 OK");
         header('Content-Type: application/json');
         http_response_code(200);
-        echo '{"messsage":"'.$msg.'"}';
+        if($msg != "undefined"){
+            echo '{"messsage":"'.$msg.'"}';
+        }
         die;
     }
 
     // Responds with 401 status and message
-    public static function ResponsenotAuthorized($msg)
+    public static function ResponseBadRequest($msg = "undefined")
+    {
+        header("HTTP/1.1 400");
+        header('Content-Type: application/json');
+        http_response_code(400);
+
+        if($msg != "undefined"){
+            echo '{"messsage":"'.$msg.'"}';
+        }
+
+        die;
+    }
+
+    // Responds with 401 status and message
+    public static function ResponsenotAuthorized($msg = "undefined")
     {
         header("HTTP/1.1 401");
         header('Content-Type: application/json');
         http_response_code(401);
-        echo '{"messsage":"'.$msg.'"}';
+
+        if($msg != "undefined"){
+            echo '{"messsage":"'.$msg.'"}';
+        }
+
         die;
     }
 }
